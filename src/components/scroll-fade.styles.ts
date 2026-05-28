83357bc4a3ef960bce6d4dde6fe03d30aaee582d
yui540 <tabletennis540@gmail.com>
2026-05-28 09:51:47 +0900

    スクロール用グラデーションの色を修正

--- src/components/scroll-fade.styles.ts
+++ src/components/scroll-fade.styles.ts
@@ -20,12 +20,12 @@ export const scrollFadeStyles = `
 
 .comimi-scrollfade::before {
   top: 0;
-  background: linear-gradient(to bottom, var(--comimi-bg), transparent);
+  background: linear-gradient(to bottom, #fff, transparent);
 }
 
 .comimi-scrollfade::after {
   bottom: 0;
-  background: linear-gradient(to top, var(--comimi-bg), transparent);
+  background: linear-gradient(to top, #fff, transparent);
 }
 
 .comimi-scrollfade[data-scroll-up="true"]::before {
